30c8e1e2670332caafce26201cbf211addb659d8
bitkiwi <varkiwibusiness@gmail.com>
2024-01-15 22:17:14 -0800

    logos and architecture

--- main.sh
+++ main.sh
@@ -1,25 +1,58 @@
+. logo.sh                                # Import logo declarations
+
+# Fetch Info
+
 declare user=$USER
 declare host=$HOSTNAME
-declare os=$(uname -o)
-declare kernel=$(uname -r)
+declare kernel="$(uname -r)"
 source /etc/os-release
-declare distro=$NAME
+declare distro="$NAME"
+declare architecture="$(uname -m)"
 declare sh=$(basename $SHELL)
-dot="\uf444"
 
+# Load a Logo
+
+case $distro in
+	"Arch Linux")
+		declare -n logo=arch
+		;;
+
+	"Ubuntu")
+		declare -n logo=ubuntu
+		;;
+
+	"Debian GNU/Linux")
+		declare -n logo=debian
+		;;
+
+	"Fedora")
+		declare -n logo=fedora
+		;;
+
+	"Linux Mint")
+		declare -n logo=mint
+		;;
+
+	"Pop!_OS")
+		declare -n logo=popos
+		;;
+	
+	*)
+		declare -n logo=na
+		;;
+esac
+
+# Visuals and Color
+
+dot="\uf444"
 pad="    "
 
-white="\033[37m"
-red="\033[31m" 
-yellow="\033[33m" 
-green="\033[32m" 
-blue="\033[34m" 
-cyan="\033[36m" 
-magenta="\033[35m" 
-black="\033[30m" 
+# Print to screen
 
-echo -e "$pad\uf007  $user@$host"
-echo -e "$pad\uf109  $kernel"
-echo -e "$pad\uf31a  $distro"
-echo -e "$pad\ue795  $sh"
-echo -e "$pad\ue22b  $white$dot $red$dot $yellow$dot $green$dot $blue$dot $cyan$dot $magenta$dot $black$dot \033[0m"
+echo
+echo -e "${logo[0]} \uf007  $user@$host"
+echo -e "${logo[1]} \uf109  $kernel"
+echo -e "${logo[2]} \uf31a  $distro $architecture"
+echo -e "${logo[3]} \ue795  $sh"
+echo -e "${logo[4]} \ue22b  $white$dot $red$dot $yellow$dot $green$dot $blue$dot $cyan$dot $magenta$dot $black$dot $reset"
+echo
