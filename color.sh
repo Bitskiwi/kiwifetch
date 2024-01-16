90d0f33401177f59d95da069da741f9543b418e4
bitkiwi <varkiwibusiness@gmail.com>
2024-01-15 22:17:35 -0800

    logos and architecture

---
+++ color.sh
@@ -0,0 +1,11 @@
+# Colors
+
+white="\033[37m"
+red="\033[31m" 
+yellow="\033[33m" 
+green="\033[32m" 
+blue="\033[34m" 
+cyan="\033[36m" 
+magenta="\033[35m" 
+black="\033[30m"
+reset="\033[0m"
---
+++ logo.sh
@@ -0,0 +1,57 @@
+. color.sh                               # Import color declarations
+
+arch=(
+	"$cyan    /\    $reset"
+	"$cyan   /\ \   $reset"
+	"$cyan  / __ \  $reset"
+	"$cyan /_|  |_\ $reset"
+	"$cyan|/      \|$reset"
+) 
+
+ubuntu=(
+	"$red   ____() $reset" 
+	"$red  /     \ $reset"   
+	"$red()|     | $reset"        
+	"$red  \____ / $reset"  
+	"$red       () $reset"
+) 
+
+debian=(
+	"$red   ____   $reset"
+	"$red  /  _ \  $reset"
+	"$red  | (__/  $reset"
+	"$red  \_      $reset"
+	"$red    \_    $reset"
+) 
+
+fedora=(
+	"$white    ___   $reset"
+	"$white   //  $blue\  $reset"
+	"$white __||__$blue/  $reset"
+	"$blue/  $white||     $reset"
+	"$white\__//     $reset"
+) 
+
+mint=(
+	"$green ________ $reset"
+	"$green|_ | ___ |$reset"
+	"$green | || | ||$reset"
+	"$green | \____/|$reset"
+	"$green  \_____/ $reset"        
+) 
+
+popos=(
+	"$cyan ____   __$reset"
+	"$cyan|  _ \ / /$reset"
+	"$cyan \  _//_/ $reset"
+	"$cyan  \_\ ()  $reset"
+	"$cyan  ======= $reset"
+)
+
+ na=(
+	"$red          $reset"
+	"$red we don't $reset"
+	"$red have this$reset"
+	"$red logo yet $reset"
+	"$red          $reset"
+)
